4a93ff04ec025726d268aa22d66cfe5391d7da66
ffpedroka-stack <ffpedroka@gmail.com>
2026-01-19 23:36:03 +0000

    CrieiCartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,57 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+// Estrutura da carta
+struct Carta {
+    char estado[20];
+    char cidade[50];
+    int populacao;
+    float area;
+    int pontos;
+};
+
+// Função para cadastrar uma carta
+void cadastrarCarta(struct Carta *c) {
+
+    printf("Nome do Estado:\n");
+    scanf("%19s", c->estado);
+
+    printf("Nome da Cidade:\n");
+    scanf("%49s", c->cidade);
+
+    printf("Numero da Populacao:\n");
+    scanf("%d", &c->populacao);
+
+    printf("Tamanho da Area:\n");
+    scanf("%f", &c->area);
+
+    printf("Pontos turisticos:\n");
+    scanf("%d", &c->pontos);
+}
+
+// Função para mostrar uma carta
+void mostrarCarta(struct Carta c, int numero) {
+
+    printf("\n--- Carta %d ---\n", numero);
+    printf("Estado: %s\n", c.estado);
+    printf("Cidade: %s\n", c.cidade);
+    printf("Populacao: %d\n", c.populacao);
+    printf("Area: %.2f\n", c.area);
+    printf("Pontos Turisticos: %d\n", c.pontos);
+}
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
 
-  // Área para entrada de dados
+    struct Carta carta1, carta2;
 
-  // Área para exibição dos dados da cidade
+    printf("Cadastro da Carta 1\n");
+    cadastrarCarta(&carta1);
 
-return 0;
-} 
+    printf("\nCadastro da Carta 2\n");
+    cadastrarCarta(&carta2);
+
+    printf("\n===== CARTAS CADASTRADAS =====\n");
+    mostrarCarta(carta1, 1);
+    mostrarCarta(carta2, 2);
+
+    return 0;
+}
